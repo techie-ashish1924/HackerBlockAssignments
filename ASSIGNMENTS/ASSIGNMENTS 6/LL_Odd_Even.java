9d39a717d064d68da2c1138699b8017549aa60f7
Ashish Sengar <techie.sengar1924@gmail.com>
2023-01-08 18:37:02 +0530

    final

--- ASSIGNMENTS/ASSIGNMENTS 6/LL_Odd_Even.java
+++ ASSIGNMENTS/ASSIGNMENTS 6/LL_Odd_Even.java
@@ -1,8 +1,5 @@
 
-public class LL_Odd_Even {
-
-
-    
+class List_Implement {
 
     class Node {
 
@@ -10,10 +7,9 @@ public class LL_Odd_Even {
         Node next;
     }
 
-    public static Node head;
+    static Node head;
 
-    public void AddNode(int ele)
-    {
+    void AddNode(int ele) {
         Node ob = new Node();
         ob.data = ele;
         ob.next = null;
@@ -30,16 +26,78 @@ public class LL_Odd_Even {
             temp.next = ob;
         }
     }
-    
-    public void Display()
-    {
+
+    void Display() {
         Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public Node EvenOdd()
+    {
+        Node Even = new Node();
+        Node Even_copy = Even;
+
+        Node Odd = new Node();
+        Node Odd_copy = Odd;
+
+        Node temp = head;
+
+        while (temp != null) {
+            if (temp.data % 2 == 0) {
+                Even.next = temp;
+                Even = Even.next;
+            } else {
+                Odd.next = temp;
+                Odd = Odd.next;
+            }
+
+            temp = temp.next;
+        }
+        Even.next = null;
+        Odd.next = Even_copy.next;
+        // Even.next = Odd_copy.next;
+
+        return Odd_copy.next;
+
+    }
+    
+    public void ODD_Even_Display()
+    {
+        Node temp = EvenOdd();
 
         while(temp != null)
         {
             System.out.print(temp.data + "-->");
+            temp = temp.next;
         }
-        System.out.println();
+        System.out.println("NULL");
+    }
+}
+
+
+public class LL_Odd_Even {
+
+
+    
+
+    
+
+    public static void main(String[] args) {
+        
+        List_Implement obj = new List_Implement();
+        obj.AddNode(10);
+        obj.AddNode(21);
+        obj.AddNode(30);
+        obj.AddNode(40);
+        obj.AddNode(50);
+
+        obj.Display();
+        obj.ODD_Even_Display();
     }
 
 }
